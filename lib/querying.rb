96824baab372e1751c0e291c8cb5c6e175a0d00e
Ashley Twomey <aprier091012@gmail.com>
2020-02-14 01:44:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,8 +31,8 @@ def select_series_title_with_most_human_characters
     "SELECT Series.title
    FROM series
    JOIN books on books.series_id = series.id
-   JOIN character_books on book.id = books.id
-   JOIN 
+   JOIN character_books on character_books.book_id = books.id
+   JOIN characters on character_books.character_id = character.id
    WHERE characters.character_id = series.id and characters.species='human'
    GROUP BY Series.title
    LIMIT 1;"
